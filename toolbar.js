44607635546388d8c05880ae90a55a893978ec5d
UnheardWolf344 <griffin3868@gmail.com>
2025-05-09 00:58:21 -0400

    fix the footer

--- toolbar.js
+++ toolbar.js
@@ -1,3 +1,8 @@
-fetch("toolbar.html") // Load toolbar from an external file
+fetch("templates/toolbar.html") // Load toolbar from an external file
   .then(response => response.text())
   .then(data => document.getElementById("toolbar").innerHTML = data);
+
+// get the footer
+fetch("templates/footer.html")
+  .then(response => response.text())
+  .then(data => document.getElementById("footer").innerHTML = data);
