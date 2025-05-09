95c78472c99ff6a1f9a5a4301c521850753853d3
UnheardWolf344 <griffin3868@gmail.com>
2025-05-09 16:53:01 -0400

    add the favicon to everything
    and do other things
    like
    
    uhh
    
    um
    
    do things
    
    like
    
    fix the logo

--- toolbar.js
+++ toolbar.js
@@ -6,7 +6,4 @@ fetch("https://bitflip.club/templates/toolbar.html") // Load toolbar from an ext
 fetch("https://bitflip.club/templates/footer.html")
   .then(response => response.text())
   .then(data => document.getElementById("footer").innerHTML = data);
-
-// const videoHeight = (document.getElementById("video").clientHeight);
-// 
-// document.getElementById("main-header").clientHeight = videoHeight;
+  
