1906bbdee8a280dd5a84fb259db1bf6aacfc5a95
UnheardWolf344 <griffin3868@gmail.com>
2025-05-10 01:10:18 -0400

    fix toolbar loading and add dropdown support

--- toolbar.js
+++ toolbar.js
@@ -1,9 +1,28 @@
-fetch("https://bitflip.club/templates/toolbar.html") // Load toolbar from an external file
+fetch("/templates/toolbar.html") // Load toolbar from an external file
   .then(response => response.text())
   .then(data => document.getElementById("toolbar").innerHTML = data);
 
 // get the footer
-fetch("https://bitflip.club/templates/footer.html")
+fetch("/templates/footer.html")
   .then(response => response.text())
   .then(data => document.getElementById("footer").innerHTML = data);
   
+
+
+function toggleDropdown (element) {
+  let dropdown = element.parentElement.querySelector(".dropdown-content");
+  dropdown.style.display = (dropdown.style.display == "block") ? "none" : "block";
+  console.log("click");
+}
+
+window.onclick = function(element) {
+  if (!event.target.matches(".dropdown-btn")) {
+    let dropdowns = document.getElementsByClassName(".dropdown-content");
+    for (let i = 0; i < dropdowns.length; i++) {
+      let thisDropdown = dropdowns[i];
+      if (thisDropdown.style.display == "block") {
+        thisDropdown.style.display = "none";
+      }
+    }
+  }
+}
